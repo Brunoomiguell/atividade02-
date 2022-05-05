daec32cb25b8d0d03fc178cdf3f475e1fb687fa4
Brunoomiguell <103513090+Brunoomiguell@users.noreply.github.com>
2022-05-05 10:51:35 -0300

    Add files via upload

---
+++ atividade02.js
@@ -0,0 +1,29 @@
+const teunome= 'Bruno Miguel' ;
+
+function nome(){
+    console.log (teunome)
+    return
+}
+
+nome()
+
+function soma (a,b){
+    return console.log(a + b )
+
+}
+
+soma(3,5)
+
+function mult (c,d){
+    return console.log(c * d )
+
+}
+
+mult(3,5)
+
+function media (a,b,c,d){
+let res = (a + b + c + d )/4
+ console.log(res)
+ return
+}
+ media(9,8,4,3)
